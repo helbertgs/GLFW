2fb43bb540b0e7d66a0b766e8cf340c70b865a25
Helbert Gomes <helbertgs@gmail.com>
2024-06-08 02:04:54 -0400

    fix: try to fix a problem to compile on windows

---
+++ Sources/GLFW/include/gl.h
@@ -0,0 +1,7 @@
+#pragma once
+#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32) || defined(__MINGW32__)
+#include <windows.h>
+#include <GL/gl.h>
+#include "glfw3.h"
+#include "glfw3native.h"
+#endif
